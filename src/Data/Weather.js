499e1792c9260cd05a8ad5fcb1e02f5fbceea874
Ashabil Syauqi <ashabilsyauqi@gmail.com>
2023-08-22 15:26:30 +0700

    solved code

--- src/Data/Weather.js
+++ src/Data/Weather.js
@@ -2,31 +2,30 @@ import React from 'react';
 
 export default function Weather({ weatherData }) {
   if (!weatherData) {
-    // alert("Maaf Kota tidak ditemukan...");
-    // return null; // Menghentikan rendering komponen lebih lanjut
+    
+    return <p>tidak ada hasil</p>;
   }
 
   const iconBaseUrl = "https://openweathermap.org/img/wn/";
-  const iconCode = weatherData.weather[0].icon;
-  const iconUrl = `${iconBaseUrl}${iconCode}@2x.png`;
+  
+  // Pastikan ada data weather sebelum mengakses properti weather[0]
+  if (weatherData.weather && weatherData.weather.length > 0) {
+    const iconCode = weatherData.weather[0].icon;
+    const iconUrl = `${iconBaseUrl}${iconCode}@2x.png`;
 
-  const tempCelsius = (weatherData.main.temp - 273.15).toFixed(2);
+    const tempCelsius = (weatherData.main.temp - 273.15).toFixed(2);
 
-  return (
-    <div className='text-left'>
-      <div className='images'>
-        <img src={iconUrl} alt='weather-img' className='weather-img' />
+    return (
+      <div className='text-left'>
+        <div className='images'>
+          <img src={iconUrl} alt='weather-img' className='weather-img' />
+        </div>
+        <h1 className='weather-status'>{weatherData.weather[0].description}</h1>
+        <p className='weather-location'>Location: {weatherData.name}, {weatherData.sys.country}</p>
+        <p className='weather-temp'>Temp: {tempCelsius} °C</p>
       </div>
-      <h1 className='weather-status'>{weatherData.weather[0].description}</h1>
-      <p className='weather-location'>location : {weatherData.name}</p>
-      <p className='weather-location'>Nation : {weatherData.sys.country}</p>
-      <p className='weather-temp'>Temp : {tempCelsius} °C</p>
-      {/* <p className='weather-temp'>Humidity : {weatherData.main[0].humidity}</p> */}
-    </div>
-  );
+    );
+  } else {
+    return <p>No weather data available.</p>;
+  }
 }
-
-
-
-
-
